37c1592f7f1b768847a9593a75f7b4c3ed4e052a
ziankork <zian@cork.technology>
2025-02-26 12:40:20 +0800

    chore: add boilerplate for transfer helpers

---
+++ src/lib/TransferHelper.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.28;
+
+
+
+library TransferHelper {
+    function safeApprove(address token, address to, uint value) internal {
+    }
+
+    function safeTransfer(address token, address to, uint value) internal {}}
