a71911f07b1ef4de94a8dfc82895f72f664b92c9
ziankork <zian@cork.technology>
2025-02-26 13:49:47 +0800

    feat: added kyberswap related stuff and remove unnecessary functions

--- src/CorkRouterV1.sol
+++ src/CorkRouterV1.sol
@@ -6,5 +6,7 @@ import {TransferHelper} from "./lib/TransferHelper.sol";
 contract CorkRouterV1 is State {
     using TransferHelper for address;
 
-    constructor(address __core, address __flashSwapRouter, address __hook) State(__core, __flashSwapRouter, __hook) {}
+    constructor(address __core, address __flashSwapRouter, address __hook, address __kyberSwapRouter)
+        State(__core, __flashSwapRouter, __hook, __kyberSwapRouter)
+    {}
 }
--- src/interfaces/IMetaAggregationRouter.sol
+++ src/interfaces/IMetaAggregationRouter.sol
@@ -24,57 +24,20 @@ interface MetaAggregationRouterV2 {
         bytes clientData;
     }
 
-    event ClientData(bytes clientData);
-
-    event Error(string reason);
-
-    event Exchange(address pair, uint256 amountOut, address output);
-    
-    event Fee(
-        address token, uint256 totalAmount, uint256 totalFee, address[] recipients, uint256[] amounts, bool isBps
-    );
-    
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-    
-    event Swapped(
-        address sender,
-        address srcToken,
-        address dstToken,
-        address dstReceiver,
-        uint256 spentAmount,
-        uint256 returnAmount
-    );
-
-    receive() external payable;
-
-    function WETH() external view returns (address);
-    
-    function isWhitelist(address) external view returns (bool);
-    
-    function owner() external view returns (address);
-    
-    function renounceOwnership() external;
-    
-    function rescueFunds(address token, uint256 amount) external;
-   
     function swap(SwapExecutionParams memory execution)
         external
         payable
         returns (uint256 returnAmount, uint256 gasUsed);
-   
+
     function swapGeneric(SwapExecutionParams memory execution)
         external
         payable
         returns (uint256 returnAmount, uint256 gasUsed);
-   
+
     function swapSimpleMode(
         address caller,
         SwapDescriptionV2 memory desc,
         bytes memory executorData,
         bytes memory clientData
     ) external returns (uint256 returnAmount, uint256 gasUsed);
-   
-    function transferOwnership(address newOwner) external;
-   
-    function updateWhitelist(address[] memory addr, bool[] memory value) external;
 }
