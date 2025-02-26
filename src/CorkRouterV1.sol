57ec4fdab594b7271e6531faa2eddf6bf849a2d5
ziankork <zian@cork.technology>
2025-02-26 13:05:13 +0800

    chore: added sage transfers utility

--- src/CorkRouterV1.sol
+++ src/CorkRouterV1.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.8.28;
 
 import {State} from "./State.sol";
+import {TransferHelper} from "./lib/TransferHelper.sol";
 
 contract CorkRouterV1 is State {
+    using TransferHelper for address;
+
     constructor(address __core, address __flashSwapRouter, address __hook) State(__core, __flashSwapRouter, __hook) {}
+
 }
--- src/lib/TransferHelper.sol
+++ src/lib/TransferHelper.sol
@@ -1,9 +1,20 @@
 pragma solidity ^0.8.28;
 
-
+import {SafeERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
+import {IERC20} from "openzeppelin-contracts/contracts/interfaces/IERC20.sol";
 
 library TransferHelper {
-    function safeApprove(address token, address to, uint value) internal {
+    using SafeERC20 for IERC20;
+
+    function approve(address token, address to, uint256 value) internal {
+        IERC20(token).safeIncreaseAllowance(to, value);
     }
 
-    function safeTransfer(address token, address to, uint value) internal {}}
+    function transfer(address token, address to, uint256 value) internal {
+        IERC20(token).safeTransfer(to, value);
+    }
+
+    function transferFrom(address token, address from, address to, uint256 value) internal {
+        IERC20(token).safeTransferFrom(from, to, value);
+    }
+}
