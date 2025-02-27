4533beebaae5e1324fffa1f9f779920b602fe049
ziankork <zian@cork.technology>
2025-02-27 16:26:17 +0800

    feat: added deposits function

--- src/CorkRouterV1.sol
+++ src/CorkRouterV1.sol
@@ -1,12 +1,34 @@
 pragma solidity ^0.8.28;
 
 import {State} from "./State.sol";
+import {AbtractAction} from "./AbstractAction.sol";
 import {TransferHelper} from "./lib/TransferHelper.sol";
+import {ICorkSwapAggregator} from "./interfaces/ICorkSwapAggregator.sol";
+import {Id} from "Depeg-swap/contracts/libraries/Pair.sol";
 
-contract CorkRouterV1 is State {
+contract CorkRouterV1 is State, AbtractAction {
     using TransferHelper for address;
 
-    constructor(address __core, address __flashSwapRouter, address __hook, address __kyberSwapRouter)
-        State(__core, __flashSwapRouter, __hook, __kyberSwapRouter)
-    {}
+    constructor(address __core, address __flashSwapRouter, address __hook) State(__core, __flashSwapRouter, __hook) {}
+
+    function depositPsm(ICorkSwapAggregator.SwapParams calldata params, Id id) external {
+        uint256 amount = _swap(params);
+
+        _increaseAllowanceForProtocol(params.tokenOut, amount);
+
+        _psm().depositPsm(id, amount);
+
+        _transferAllCtDsToUser(id);
+    }
+
+    function depositLv(ICorkSwapAggregator.SwapParams calldata params, Id id, uint256 raTolerance, uint256 ctTolerance)
+        external
+    {
+        uint256 amount = _swap(params);
+
+        _increaseAllowanceForProtocol(params.tokenOut, amount);
+        _vault().depositLv(id, amount, raTolerance, ctTolerance);
+
+        _transferAllLvToUser(id);
+    }
 }
