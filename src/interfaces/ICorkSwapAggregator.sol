e4583a7fe645ef0a6686334b95f3518cdb226969
ziankork <zian@cork.technology>
2025-02-28 16:42:02 +0800

    chore: add router data for redeem lv

--- src/interfaces/ICorkSwapAggregator.sol
+++ src/interfaces/ICorkSwapAggregator.sol
@@ -1,5 +1,7 @@
 pragma solidity ^0.8.26;
 
+import {Id} from "Depeg-swap/contracts/libraries/Pair.sol";
+
 interface ICorkSwapAggregator {
     struct SwapParams {
         address tokenIn;
@@ -8,6 +10,16 @@ interface ICorkSwapAggregator {
         uint256 amountOutMin;
         address extRouter;
         bytes extRouterData;
+    }
+
+    // pass this as bytes data when redeeming LV via the router
+    struct RouterParams {
+        Id id;
+        // frontend should do the same logic as the contract and then
+        // do a sell DS preview to get this value
+        uint256 dsMinOut;
+        // aggregator swap data for pa token -> RA
+        ICorkSwapAggregator.SwapParams paSwapAggregatorData;
     }
 
     function swap(SwapParams calldata params) external returns (uint256 amountOut);
