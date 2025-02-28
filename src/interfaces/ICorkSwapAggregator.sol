05b3e2bb7b23aebcf57c3148136e6c89e9ac01ee
ziankork <zian@cork.technology>
2025-03-01 00:20:17 +0800

    feat: update interfaces

--- src/interfaces/ICorkSwapAggregator.sol
+++ src/interfaces/ICorkSwapAggregator.sol
@@ -14,12 +14,17 @@ interface ICorkSwapAggregator {
 
     // pass this as bytes data when redeeming LV via the router
     struct RouterParams {
+        address receiver;
         Id id;
         // frontend should do the same logic as the contract and then
         // do a sell DS preview to get this value
         uint256 dsMinOut;
         // aggregator swap data for pa token -> RA
+        // the amount in should include the amount of idle PA and the PA we got from redeeming CT(if the CT is expired)
         ICorkSwapAggregator.SwapParams paSwapAggregatorData;
+        // if set to false then the cork router will not use external router to convert the PA -> RA
+        // so user instead will get both RA + PA
+        bool useExtRouter;
     }
 
     function swap(SwapParams calldata params) external returns (uint256 amountOut);
