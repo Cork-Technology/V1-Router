c1e3f41e26d500e14542f17d5431b57c9cde5388
siddhpurakaran <siddhpurakaran.sk@gmail.com>
2025-03-05 16:39:57 +0530

    chore : solhint errors fixed

--- src/CorkRouterV1.sol
+++ src/CorkRouterV1.sol
@@ -2,13 +2,13 @@
 pragma solidity ^0.8.26;
 
 import {State} from "./State.sol";
-import {AbtractAction} from "./AbstractAction.sol";
+import {AbstractAction} from "./AbstractAction.sol";
 import {ICorkSwapAggregator} from "./interfaces/ICorkSwapAggregator.sol";
 import {Id} from "Depeg-swap/contracts/libraries/Pair.sol";
 import {IWithdrawalRouter} from "Depeg-swap/contracts/interfaces/IWithdrawalRouter.sol";
 import {IDsFlashSwapCore} from "Depeg-swap/contracts/interfaces/IDsFlashSwapRouter.sol";
 
-contract CorkRouterV1 is State, AbtractAction, IWithdrawalRouter {
+contract CorkRouterV1 is State, AbstractAction, IWithdrawalRouter {
     constructor(address __core, address __flashSwapRouter, address __hook) State(__core, __flashSwapRouter, __hook) {}
 
     function depositPsm(ICorkSwapAggregator.AggregatorParams calldata params, Id id)
